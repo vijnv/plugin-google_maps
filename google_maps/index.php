ab28c66d1a62ae011a449b96e08ffae25dd9908f
Vincent Vijn <vincent@vijn.me>
2023-12-15 23:11:48 +0100

    Rewrite v3

--- google_maps/index.php
+++ google_maps/index.php
@@ -1,71 +1,73 @@
 <?php
 /*
 Plugin Name: Google Maps
-Plugin URI: http://www.osclass.org/
+Plugin URI: https://github.com/vijnv/plugin-google_maps
 Description: This plugin shows a Google Map on the location space of every item.
-Version: 2.1.7
-Author: Osclass & kingsult
-Author URI: http://www.osclass.org/
-Plugin update URI: http://www.osclass.org/files/plugins/google_maps/update.php
+Version: 3.0.0
+Author: Osclass, kingsult, Vincent Vijn
+Author URI: https://github.com/vijnv/plugin-google_maps
+Plugin update URI:
 */
 
-    function google_maps_location() {
-        $item = osc_item();
-        osc_google_maps_header();
-        require 'map.php';
+function google_maps_install()
+{
+    osc_set_preference('maps_key', '', 'google_maps', 'STRING');
+    osc_set_preference('maps_server_key', '', 'google_maps', 'STRING');
+    osc_reset_preferences();
+}
+
+function google_maps_uninstall()
+{
+    osc_delete_preference('maps_key', 'google_maps');
+    osc_delete_preference('maps_server_key', 'google_maps');
+    osc_reset_preferences();
+}
+
+
+function google_maps_location() {
+    $item = osc_item();
+    require 'map.php';
+}
+
+function insert_geo_location($item) {
+    $itemId = $item['pk_i_id'];
+    $location = google_maps_fetch_geo($item);
+
+    if ($location !== false) {    
+        ItemLocation::newInstance()->update (array('d_coord_lat' => $location->lat,
+                                                   'd_coord_long' => $location->lng)
+                                            ,array('fk_i_item_id' => $itemId));
     }
+}
 
-    // HELPER
-    function osc_google_maps_header() {
-        echo '<script src="https://maps.google.com/maps/api/js?sensor=false&key='.osc_get_preference('maps_key', 'google_maps').'" type="text/javascript"></script>';
-        echo '<style>#itemMap img { max-width: 140em; } </style>';
+function google_maps_fetch_geo($item) {
+    $sAddress = (isset($item['s_address']) ? $item['s_address'] : '');
+    $sCity = (isset($item['s_city']) ? $item['s_city'] : '');
+    $sRegion = (isset($item['s_region']) ? $item['s_region'] : '');
+    $sCountry = (isset($item['s_country']) ? $item['s_country'] : '');
+    $address = sprintf('%s, %s, %s, %s', $sAddress, $sCity, $sRegion, $sCountry);
+
+    $response = osc_file_get_contents(sprintf('https://maps.googleapis.com/maps/api/geocode/json?address=%s&key='.osc_get_preference('maps_server_key', 'google_maps'), urlencode($address)));
+    $jsonResponse = json_decode($response);
+    
+    if (isset($jsonResponse->results[0]->geometry->location))       {
+        $location = $jsonResponse->results[0]->geometry->location;
+        return $location;
+    } else {
+        error_log("Failed to insert geo location for item ".$item['pk_i_id'].". Response: ".$response);
+        return false;
     }
+}
 
-    function insert_geo_location($item) {
-        $itemId = $item['pk_i_id'];
-        $aItem = Item::newInstance()->findByPrimaryKey($itemId);
-        $sAddress = (isset($aItem['s_address']) ? $aItem['s_address'] : '');
-        $sCity = (isset($aItem['s_city']) ? $aItem['s_city'] : '');
-        $sRegion = (isset($aItem['s_region']) ? $aItem['s_region'] : '');
-        $sCountry = (isset($aItem['s_country']) ? $aItem['s_country'] : '');
-        $address = sprintf('%s, %s, %s, %s', $sAddress, $sCity, $sRegion, $sCountry);
-        $response = osc_file_get_contents(sprintf('http://maps.googleapis.com/maps/api/geocode/json?address=%s&sensor=false', urlencode($address)));
-        $jsonResponse = json_decode($response);
-        
-        if (isset($jsonResponse->results[0]->geometry->location) && count($jsonResponse->results[0]->geometry->location) > 0) 		{
-        	$location = $jsonResponse->results[0]->geometry->location;
-        	$lat = $location->lat;
-        	$lng = $location->lng;
-        
-            ItemLocation::newInstance()->update (array('d_coord_lat' => $lat
-                                                      ,'d_coord_long' => $lng)
-                                                ,array('fk_i_item_id' => $itemId));
-        }
-    }
+osc_add_hook('location', 'google_maps_location');
+osc_add_hook('posted_item', 'insert_geo_location');
+osc_add_hook('edited_item', 'insert_geo_location');
 
-    osc_add_hook('init',function() {
-        if (osc_get_preference('include_maps_js', 'google_maps') != '0') {
-            osc_add_hook('location', 'google_maps_location');
-        }
-    } );
+osc_add_route('google_maps_settings', 'google_maps_settings', 'google_maps_settings', 'google_maps/admin/settings.php');
+osc_add_hook('admin_menu_init', function() {
+    osc_add_admin_submenu_divider('plugins', 'Google Maps Plugin', 'google_maps_divider', 'administrator');
+    osc_add_admin_submenu_page('plugins', __('Settings', 'google_maps'), osc_route_admin_url('google_maps_settings'), 'google_maps_setting', 'administrator');
+});
 
-    osc_add_hook('posted_item', 'insert_geo_location');
-    osc_add_hook('edited_item', 'insert_geo_location');
-
-    osc_add_route('google_maps_settings', 'google_maps_settings', 'google_maps_settings', 'google_maps/admin/settings.php');
-    osc_add_hook('admin_menu_init', function() {
-        osc_add_admin_submenu_divider('plugins', 'Google Maps Plugin', 'google_maps_divider', 'administrator');
-        osc_add_admin_submenu_page('plugins', __('Settings', 'google_maps'), osc_route_admin_url('google_maps_settings'), 'google_maps_setting', 'administrator');
-    });
-
-
-    osc_add_hook('admin_header',  function() {
-        if(Params::getParam('route')=='google_maps_settings') osc_remove_hook('admin_page_header', 'customPageHeader');
-    });
-    osc_add_hook('admin_page_header',  function() {
-        if (Params::getParam('route') == 'google_maps_settings') {
-            ?>
-            <h1><?php _e('Google Maps Plugin', 'google_maps'); ?></h1>
-            <?php
-        }
-    });
+osc_register_plugin(osc_plugin_path(__FILE__), 'google_maps_install');
+osc_add_hook(osc_plugin_path(__FILE__)."_uninstall", 'google_maps_uninstall');
